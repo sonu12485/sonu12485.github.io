ef624901b53af00974de9754e9dcadb3ea053830
sonu <sonusaikishan@gmail.com>
2018-03-26 21:58:32 +0530

    Updated my portfolio added hpdf certificate and hobby

--- js/init.js
+++ js/init.js
@@ -18,33 +18,3 @@ $(document).ready(function(){
 
 })(jQuery);
 
-var string = "K.SAI KISHAN";
-var array = string.split("");
-var timer;
-
-function frameLooper () {
-	if (array.length > 0) {
-		document.getElementById("text").innerHTML += array.shift();
-	} else {
-		clearTimeout(timer);
-			}
-	loopTimer = setTimeout('frameLooper()',100);
-
-}
-frameLooper();
-
-var string1 = "WEB DEVELOPER & DESIGNER";
-var array1 = string1.split("");
-var timer1;
-
-function frameLooper1 () {
-	if (array1.length > 0) {
-		document.getElementById("text1").innerHTML += array1.shift();
-	} else {
-		clearTimeout(timer1);
-			}
-	loopTimer = setTimeout('frameLooper1()',80);
-
-}
-
-setTimeout(frameLooper1,1300);
